590b484ee09e8ce47b3232e6c8a9317b94adac43
AJ <87444623+AJ224@users.noreply.github.com>
2023-02-17 14:22:37 +0530

    Create gps.py

---
+++ GPS/gps.py
@@ -0,0 +1,13 @@
+#https://www.cyberpunk.rs/building-raspberry-pi-gui
+
+import serial              
+from time import sleep
+import sys
+
+ser = serial.Serial (“/dev/ttyS0”)
+try:
+    while True:
+        received_data = (str)(ser.readline()) #read NMEA string received
+        print(received_data, “\n”)
+except KeyboardInterrupt:
+    sys.exit(0)  
